1b936a0781d8588fad7c5ac505c754f27f4a1d0b
ondrejmichejda <ondra.michejda@gmail.com>
2021-05-18 16:47:50 +0200

    init

---
+++ E_Lock_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E_Lock_Console
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
